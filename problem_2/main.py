1e018ad592904d3d053a1bb27b40fc6f2139a089
Jamie Place <jamieplace95@gmail.com>
2023-12-12 23:47:24 +0000

    few project updates

--- problem_2/main.py
+++ problem_2/main.py
@@ -1,7 +1,10 @@
 from pathlib import Path
 import math
 
-max_allowable = {"red":12, "green":13, "blue":14}
+
+
+max_allowable = {"red": 12, "green": 13, "blue": 14}
+
 
 def read_file():
     path = Path(__file__).parent / "input.txt"
@@ -10,11 +13,12 @@ def read_file():
     for line in lines:
         yield line
 
+
 def part1():
     strings = read_file()
     game_counts = {}
     for i, string in enumerate(strings):
-        max_values = {"red":-math.inf, "green":-math.inf, "blue":-math.inf}
+        max_values = {"red": -math.inf, "green": -math.inf, "blue": -math.inf}
         string = string.replace(" ", "")
         game_id, subsets = string.split(":")
         # remove the 'Game' from the string
@@ -27,19 +31,20 @@ def part1():
                 colour = "".join(filter(str.isalpha, ball))
                 if int(count) > max_values[colour]:
                     max_values[colour] = int(count)
-        game_counts[i+1] = max_values
+        game_counts[i + 1] = max_values
         for key, value in max_values.items():
             if value > max_allowable[key]:
-                del game_counts[i+1]
+                del game_counts[i + 1]
                 break
     return sum(game_counts.keys())
+
 
 def part2():
     strings = read_file()
     game_counts = {}
     game_power = []
     for i, string in enumerate(strings):
-        max_values = {"red":-math.inf, "green":-math.inf, "blue":-math.inf}
+        max_values = {"red": -math.inf, "green": -math.inf, "blue": -math.inf}
         string = string.replace(" ", "")
         game_id, subsets = string.split(":")
         # remove the 'Game' from the string
@@ -52,14 +57,13 @@ def part2():
                 colour = "".join(filter(str.isalpha, ball))
                 if int(count) > max_values[colour]:
                     max_values[colour] = int(count)
-        game_counts[i+1] = max_values
+        game_counts[i + 1] = max_values
 
     for _, value in game_counts.items():
         game_power.append(value["red"] * value["green"] * value["blue"])
     return sum(game_power)
 
-        
+
 if __name__ in "__main__":
     print("Part 1: ", part1())
     print("Part 2: ", part2())
-    
